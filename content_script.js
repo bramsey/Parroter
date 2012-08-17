a562b839a06be72f6de60f918d4a4f8522b1c521
Bill Ramsey <bill@billiamram.com>
2012-08-17 13:30:22 -0400

    added training data and data store

--- content_script.js
+++ content_script.js
@@ -1,3 +1,6 @@
+// This job of this script is to get the current site privacy policy's text
+// and send it to the background script for analysis.
+
 var bodyText = document.body.innerText;
 
 chrome.extension.sendRequest({bodyText: bodyText}, function(response) {});
